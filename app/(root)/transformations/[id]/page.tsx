26b495f26940a4a6e26ce6078f44b2cd33fbc986
shaik-adnan1 <yousufwrk0@gmail.com>
2024-02-17 20:21:36 +0530

    Mid Commit  -> Folder Structure update -> Routing setup done -> navigation

---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TransformationUpdatePage = ({}) => {
+  return (
+    <div>
+      Transformation Update Page
+    </div>
+  )
+}
+
+export default TransformationUpdatePage;
---
+++ app/(root)/transformations/[id]/update/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UpdateTransformation = () => {
+  return (
+    <div>
+      Update Transformation page
+    </div>
+  )
+}
+
+export default UpdateTransformation;
